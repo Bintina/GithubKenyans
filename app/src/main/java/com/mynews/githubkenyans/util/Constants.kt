4b934262db2cfa035374ba1653e02f36a616db34
Prateek Prasad <leavethemails@gmail.com>
2023-04-23 21:01:42 +0530

    cleanup activity

--- app/src/main/java/com/mynews/githubkenyans/util/Constants.kt
+++ app/src/main/java/com/mynews/githubkenyans/util/Constants.kt
@@ -1,9 +1,6 @@
 package com.mynews.githubkenyans.util
 
-import com.mynews.githubkenyans.model.NrbJavaDeveloper
-
 object Constants {
-    const val BASE_URL ="https://api.github.com"
-    const val NRB_JAVA_END_URL ="/search/users?q=language:java+location:nairobi"
-    lateinit var myDataset: NrbJavaDeveloper
+    const val BASE_URL = "https://api.github.com"
+    const val NRB_JAVA_END_URL = "/search/users?q=language:java+location:nairobi"
 }
